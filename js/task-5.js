fb1c570f6345d804e1790e9eaa21ecb5482294cb
Andrey Shemet <an-shem@ukr.net>
2020-12-03 11:09:24 +0200

    Исправил 5-е задание. Добавил варианты в 6-е.
    В 5-м задании исправил ошибку при нажатии отмены.
    В 6-м задании добавил еще несколько вариантов решия  и проверку если введено не число.

--- js/task-5.js
+++ js/task-5.js
@@ -1,23 +1,28 @@
-const countryForDelivery = prompt('Укажите страну доставки:').toLowerCase();
+let countryForDelivery = prompt('Укажите страну доставки:');
+if (countryForDelivery !== null) {
+  countryForDelivery = countryForDelivery.toLowerCase();
 
-switch (countryForDelivery) {
-  case 'китай':
-    alert(`Доставка в Китай будет стоить 100 кредитов`);
-    break;
-  case 'чили':
-    alert(`Доставка в Чили будет стоить 250 кредитов`);
-    break;
-  case 'австралия':
-    alert(`Доставка в Австралию будет стоить 170 кредитов`);
-    break;
-  case 'индия':
-    alert(`Доставка в Индию будет стоить 80 кредитов`);
-    break;
-  case 'ямайка':
-    alert(`Доставка в Ямайку будет стоить 120 кредитов`);
-    break;
-  default:
-    alert('В вашей стране доставка не доступна');
+  switch (countryForDelivery) {
+    case 'китай':
+      alert(`Доставка в Китай будет стоить 100 кредитов`);
+      break;
+    case 'чили':
+      alert(`Доставка в Чили будет стоить 250 кредитов`);
+      break;
+    case 'австралия':
+      alert(`Доставка в Австралию будет стоить 170 кредитов`);
+      break;
+    case 'индия':
+      alert(`Доставка в Индию будет стоить 80 кредитов`);
+      break;
+    case 'ямайка':
+      alert(`Доставка в Ямайку будет стоить 120 кредитов`);
+      break;
+    default:
+      alert('В вашей стране доставка не доступна');
+  }
+} else {
+  alert(`Отменено пользователем`);
 }
 
 // Китай - 100 кредитов
--- js/task-6.js
+++ js/task-6.js
@@ -1,13 +1,63 @@
-let input;
+//
+// ------------------------------------------------------
+// ВАРИАНТ-1 через do... while
+
+let input = 0;
 let total = 0;
 
-while (true) {
+do {
   input = prompt('Введите чило:');
+  let i = Number(input);
 
-  if (input === null) {
-    alert(`Общая сумма чисел равна ${total}`);
-    break;
+  if (Number.isNaN(i)) {
+    alert(`Было введено не число, попробуйте еще раз`);
+    continue;
   }
 
-  total += Number(input);
-}
+  total += i;
+} while (input !== null);
+
+alert(`Общая сумма чисел равна ${total}`);
+
+//
+//
+//------------------------------------------------------
+// ВАРИАНТ-2 через бесконечный цикл
+
+// let input = 0;
+// let total = 0;
+
+// while (true) {
+//   input = prompt('Введите чило:');
+
+//   if (input === null) {
+//     alert(`Общая сумма чисел равна ${total}`);
+//     break;
+//   }
+//   if (Number.isNaN(Number(input))) {
+//     alert(`Было введено не число, попробуйте еще раз`);
+//     continue;
+//   }
+
+//   total += Number(input);
+// }
+
+//
+//
+//-----------------------------------------------------------
+// ВАРИАНТ-3 через for
+
+// let input = 0;
+// let total = 0;
+
+// for (let i = 0; input !== null; total += i) {
+//   input = prompt('Введите число:');
+
+//   if (Number.isNaN(Number(input))) {
+//     alert(`Было введено не число, попробуйте еще раз`);
+//     i = 0;
+//     continue;
+//   }
+//   i = Number(input);
+// }
+// alert(`Сумма введенных Вами чисел: ${total}`);
